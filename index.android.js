710195f2957ec6aa0bddf6ea6eabc70d6ec54649
youngauto <from_yangxin@126.com>
2017-04-27 16:47:57 +0800

    ListView组件demo

--- index.android.js
+++ index.android.js
@@ -6,43 +6,111 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ListView,
+    Image,
 } from 'react-native';
 
+let MOCKED_MOVIES_DATA = [{
+    title: 'Title',
+    year: '2017',
+    posters: {
+        thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+}];
+let REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 export default class Cake extends Component {
-  render() {
-    return (
-      <View style={{backgroundColor:'white',flex: 1,marginTop: 2,flexDirection:'row',alignItems: 'center',justifyContent: 'space-around',}} >
-          <Text style={{ color: 'black', fontFamily:'iconfont',fontSize: 30 }}>&#xe603;</Text>
-          <Text style={{ color: 'red', fontFamily:'iconfont',fontSize: 30 }}>&#xe602;</Text>
-          <Text style={{ color: 'red', fontFamily:'iconfont',fontSize: 30 }}>&#xe604;</Text>
-          <Text style={{ color: 'red', fontFamily:'iconfont',fontSize: 30 }}>&#xe605;</Text>
-          <Text style={{ color: 'red', fontFamily:'iconfont',fontSize: 30 }}>&#xe600;</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            loaded: false
+        }
+    }
+    componentDidMount() {
+        this.handleFetchData();
+    }
+    handleFetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+                    loaded: true
+                });
+            })
+            .done();
+    }
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading movies......
+                </Text>
+            </View>
+        )
+    }
+    renderMovies(movie) {
+        return (
+            <View style={styles.container} >
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        )
+    }
+    render() {
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderMovies}
+                style={styles.listView}
+            />
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+    }
 });
 
 AppRegistry.registerComponent('Cake', () => Cake);
