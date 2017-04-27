cfe498067d47c27dc4428212083e7fd552c384db
youngauto <from_yangxin@126.com>
2017-04-27 19:58:37 +0800

    布局动画demo

---
+++ src/components/Cake.js
@@ -0,0 +1,108 @@
+/**
+ * Created by yx on 2017/4/27.
+ */
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ListView,
+    Image,
+} from 'react-native';
+
+let MOCKED_MOVIES_DATA = [{
+    title: 'Title',
+    year: '2017',
+    posters: {
+        thumbnail: 'http://i.imgur.com/UePbdph.jpg'
+    }
+}];
+let REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+export default class Cake extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+            loaded: false
+        }
+    }
+    componentDidMount() {
+        this.handleFetchData();
+    }
+    handleFetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+                    loaded: true
+                });
+            })
+            .done();
+    }
+    renderLoadingView() {
+        return (
+            <View style={styles.container}>
+                <Text>
+                    Loading movies......
+                </Text>
+            </View>
+        )
+    }
+    renderMovies(movie) {
+        return (
+            <View style={styles.container} >
+                <Image
+                    source={{uri: movie.posters.thumbnail}}
+                    style={styles.thumbnail}
+                />
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        )
+    }
+    render() {
+        if (!this.state.loaded) {
+            return this.renderLoadingView();
+        }
+        return (
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderMovies}
+                style={styles.listView}
+            />
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    thumbnail: {
+        width: 53,
+        height: 81,
+    },
+    rightContainer: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+    },
+    year: {
+        textAlign: 'center',
+    },
+    listView: {
+        paddingTop: 20,
+        backgroundColor: '#F5FCFF',
+    },
+});
